964e9bbfee46ec6303210de319034ec116c84414
unknown <jonathanpoblet228@gmail.com>
2024-05-29 10:22:36 -0300

    Button Contact Added

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,12 +2,14 @@ import "./main.css";
 
 export default function Main() {
   return (
-    <main className='main'>
+    <main className='main fade-in'>
       <section className='main-banner'>
         <h1>
           <span>V</span>IRTUAL <br></br> DESIGNS
         </h1>
-        <h2>SOLUCIONES DIGITALES</h2>
+        <a href='https://api.whatsapp.com/send?phone=541130565913&text=Hola,%20quiero%20consultar%20por%20un%20sitio%20web!' target='_blank'>
+          CONTÁCTANOS
+        </a>
       </section>
     </main>
   );
