97f033d405dbdeafe8253d70d768d511ad79aece
Jonathan Poblet <jonathanpoblet228@gmail.com>
2024-10-16 20:05:34 -0300

    Some changes added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,13 +1,15 @@
-import "./banner.css";
+import './banner.css';
 
 export default function Banner() {
   return (
     <section className='banner'>
-      <h1>
-        <span>V</span>IRTUAL <br></br> DESIGNS
-      </h1>
-      <a href='https://api.whatsapp.com/send?phone=541130565913&text=Hola,%20quiero%20consultar%20por%20un%20sitio%20web!' target='_blank'>
-        CONTÁCTANOS
+      <h1>WE GROW BUSINESS ONLINE</h1>
+      <h2>Let's Build Your Website Together!</h2>
+      <a
+        href='https://api.whatsapp.com/send?phone=541130565913&text=Hola,%20quiero%20consultar%20por%20un%20sitio%20web!'
+        target='_blank'
+      >
+        CONTACT US
       </a>
     </section>
   );
