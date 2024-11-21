451564822411d41859b038defb5c575e4e50721d
unknown <jonathanpoblet228@gmail.com>
2024-11-21 14:34:16 -0300

    Updated Virtual Designs Page

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,9 +3,9 @@ import './banner.css';
 export default function Banner() {
   return (
     <section className='banner'>
-      <h1>IMPULSAMOS TU NEGOCIO EN EL MUNDO DIGITAL</h1>
+      <h1>CREAMOS WEBS QUE IMPULSAN NEGOCIOS</h1>
       <h2>
-        Construyamos Tu <b>Sitio Web</b> Juntos!
+        Construyamos Tu <b style={{color: '#000'}}>Sitio Web</b> Juntos!
       </h2>
       <a
         href='https://api.whatsapp.com/send?phone=541130565913&text=Hola,%20quiero%20consultar%20por%20un%20sitio%20web!'
